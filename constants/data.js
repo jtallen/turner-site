f67d22b6ca4f805768f0342f01334f3a45a75f5b
turner <jturnerallen@gmail.com>
2021-11-16 21:40:38 -0500

    Begun customization

--- constants/data.js
+++ constants/data.js
@@ -1,11 +1,11 @@
 const userData = {
-  githubUsername: "manuarora700",
-  name: "Manu Arora",
+  githubUsername: "jtallen",
+  name: "Turner Allen",
   designation: "Full-Stack Developer",
   avatarUrl: "/avatar.png",
-  email: "manuarorawork@gmail.com",
-  phone: "+91 9587738861",
-  address: "Jaipur, Rajasthan, India.",
+  email: "jturnerallen@gmail.com",
+  phone: "+1 8042295228",
+  address: "San Francisco, USA.",
   projects: [
     {
       title: "Tailwind Master Kit",
@@ -49,12 +49,11 @@ const userData = {
     },
   ],
   about: {
-    title:
-      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    title: "I'm a software engineer... [pull from resume]",
     description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `Some more info about me ;)`,
+      `This is the perfect place to include some more info about me!`,
+      `I know how to code!`,
     ],
     currentProject: "Tailwind Master Kit",
     currentProjectUrl: "https://tailwindmasterkit.com",
@@ -106,10 +105,10 @@ const userData = {
   resumeUrl:
     "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
   socialLinks: {
-    instagram: "https://instagram.com/maninthere",
-    twitter: "https://twitter.com/mannupaaji",
-    linkedin: "https://linkedin.com/in/manuarora28",
-    github: "https://github.com/manuarora700",
+    instagram: "https://instagram.com/jturnerallen",
+    twitter: "https://twitter.com/804native",
+    linkedin: "https://linkedin.com/in/jturnerallen",
+    github: "https://github.com/jtallen",
     facebook: "https://facebook.com/",
   },
 };
