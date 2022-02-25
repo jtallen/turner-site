2b755ad6b766f95bd50b54a752b2585782a8bb0e
turner <jturnerallen@gmail.com>
2022-02-24 16:47:23 -0800

    implemented redux to handle state management, updated some packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
-import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
-import { init } from "@emailjs/browser";
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import { init } from '@emailjs/browser';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 // Initialize emailjs lib
-init("user_BLQMNLMuIbHGtGh5Gktsy");
+init('user_BLQMNLMuIbHGtGh5Gktsy');
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="light" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="light" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
