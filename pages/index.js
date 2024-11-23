33c2e3c0759e19d1945b98ef3efa868018b6d24e
Turner Allen <jturnerallen@gmail.com>
2024-11-23 00:51:52 -0500

    cleaned, updated, and fixed github repo display + logic

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import ContainerBlock from '../components/ContainerBlock';
-import FavouriteProjects from '../components/FavouriteProjects';
-import LatestCode from '../components/LatestCode';
-import Hero from '../components/Hero';
-import getLatestRepos from '@lib/getLatestRepos';
-import userData from '@constants/data';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
 
 export default function Home({ repositories }) {
   return (
@@ -21,11 +21,9 @@ export default function Home({ repositories }) {
 }
 
 export const getServerSideProps = async () => {
-  console.log(process.env.GITHUB_AUTH_TOKEN);
   let token = process.env.GITHUB_AUTH_TOKEN;
 
   const repositories = await getLatestRepos(userData, token);
-  // console.log("REPOSITORIES", repositories);
 
   return {
     props: {
